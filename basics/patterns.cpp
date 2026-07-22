4cb3c91e4bf3d7c2fccabba88ce60d6cf5ea31a8
Aryan Singh <stupidcaptainlevi@gmail.com>
2026-07-22 12:46:14 +0530

    patterns1

---
+++ basics/patterns.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // even sum
+    // int n;
+    // cin>>n;
+    // int i = 1;
+    // int sum = 0;
+    // while (i<=n)
+    // {
+    //     if (i%2==0)
+    //     {   
+    //         cout<<i<<" ";
+    //         sum+=i;
+    //     }
+    //     i++;
+    // }
+    // cout<<sum;
+
+    // prime number
+    // int num;
+    // cin>>num;
+    // int i = 2;
+    // bool prime = true;
+    // while (i<num)
+    // {
+    //     if (num%i==0)
+    //     {
+    //         prime = false;
+    //         break;
+    //     }
+    //     i++;
+    // }
+    // if (prime)
+    // {
+    //     cout<<"Prime number";
+    // }
+    // else{
+    //     cout<<"Not prime number";
+    // }
+    
+    // ***
+    // ***
+    // ***
+    // int n;
+    // cin>>n;
+    // int i=1;
+    // while (i<=n)
+    // {
+    //     int j=1;
+    //     while (j<=n)
+    //     {
+    //         cout<<"*";
+    //         j++;
+    //     }
+    //     cout<<"\n";
+    //     i++;
+    // }
+    
+    // 111
+    // 222
+    // 333
+    // int n;
+    // cin>>n;
+    // int i=1;
+    // while (i<=n)
+    // {
+    //     int j=1;
+    //     while (j<=n)
+    //     {
+    //         cout<<i;
+    //         j++;
+    //     }
+    //     cout<<"\n";
+    //     i++;
+        
+    // }
+
+    // 1234
+    // 1234
+    // 1234
+    // int n;
+    // cin>>n;
+    // int i=1;
+    // while (i<=n)
+    // {
+    //     int j=1;
+    //     while (j<=n)
+    //     {
+    //         cout<<j;
+    //         j++;
+    //     }
+    //     cout<<"\n";
+    //     i++;
+        
+    // }
+    
+    // 321
+    // 321
+    // 321
+    int n;
+    cin>>n;
+    int i=1;
+    while (i<=n)
+    {
+        int j=1;
+        while (j<=n)
+        {
+            cout<<n-j+1;
+            j++;
+        }
+        cout<<"\n";
+        i++;
+        
+    }
+    
+
+    
+
+}
