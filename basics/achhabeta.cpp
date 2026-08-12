50d71ebda745e90d7b68ff1ace8ad56d9bb19928
Aryan Singh <stupidcaptainlevi@gmail.com>
2026-08-12 22:39:02 +0530

    Number sys

---
+++ basics/achhabeta.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    // sum till n
+    // int n;
+    // cout<<"Enter value: ";
+    // cin>>n;
+    // int sum = 0;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     sum+=i;
+    // }
+    // cout<<"Sum till value is "<<sum;
+
+    // fibonacci using for loop
+    // int n;
+    // cout<<"Enter value: ";
+    // cin>>n;
+    // int first = 0;
+    // int sec = 1;
+    // cout<<first<<" "<<sec<<" ";
+    // for (int i = 1; i <=n-2; i++)
+    // {
+    //     int next = first + sec;
+    //     cout<<next<<" ";
+    //     first = sec;
+    //     sec = next;
+    // }
+    
+    //decimal to binary
+    // int n;
+    // cout<<"Enter decimal number: ";
+    // cin>>n;
+    // int ans = 0;
+    // int i = 0;
+    // while (n!=0)
+    // {
+    //     int bit = n & 1;
+    //     ans += (bit * pow(10,i)); 
+    //     n = n >> 1;
+    //     i++;
+    // }
+    // cout<<"Binary: "<<ans;
+
+    //binary to decimal
+    int n;
+    cout<<"Enter binary number: ";
+    cin>>n;
+    int ans = 0;
+    int i = 0;
+    while (n!=0)
+    {
+        int digit = n % 10;
+        if(digit == 1) ans += pow(2,i); 
+        n /= 10;
+        i++;
+    }
+    cout<<"Decimal: "<<ans;
+    
+}
